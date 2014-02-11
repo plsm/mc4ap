61476b6ac7ef2e84adacc1ce22c60dd4c8d4ce6c
Pedro Mariano <>
2014-02-11 14:30:39 +0000

    Final changes before removing file

--- java/ui/MercuryReference.java
+++ java/ui/MercuryReference.java
@@ -1,23 +1,28 @@
 package ui;
 
+import data.closure.SetFieldFunc;
+import data.closure.GetFieldFunc;
+
 /**
  * Represents a reference to a mercury value.  Implementors of this interface can use an instance of the class to share a reference between diferent GUI components.
  * @author pedro
  */
-public interface MercuryReference<T>
+interface MercuryReference__<D>
 {
 	/**
 	 * Set the value of this data reference.
 	 * 
 	 * @param value The new value of this data reference.
 	 */
-	public void setValue (T value);
+	public void setValue (D value);
 	/**
 	 * Get the value of this data reference.
 	 * 
 	 * @return the value of this data reference.
 	 */
-	public T getValue ();
+	public D getValue ();
+	
+	
 	/**
 	 * Get the value of some field of this data.
 	 *
@@ -25,17 +30,32 @@ public interface MercuryReference<T>
 	 *
 	 * @return the value of a field of this data.
 	 */
-	Object applyGetFunc (Object[] getFunc);
+	<F> F applyGetFieldFunc (GetFieldFunc<D, F> func);
 	/**
 	 * Set the value of some field of this data.
 	 * @param setFunc The field set function.
 	 * @param newFieldValue The new field value.
 	 */
-	void applySetFunc (Object[] setFunc, Object newFieldValue);
+	<F> boolean applySetFieldFunc (SetFieldFunc<D, F> func, F newFieldValue);
+	
 	/**
-	 * Set the value of some field of this data.  The set function returns an instance {@code maybe_error(T)}.
+	 * Get the value of some field of this data.
+	 *
+	 * @param getFunc the get function used to obtain a field value of the data.
+	 *
+	 * @return the value of a field of this data.
+	 */
+	<F> F applyGetFunc (Object[] getFunc);
+	/**
+	 * Set the value of some field of this data.
+	 * @param setFunc The field set function.
+	 * @param newFieldValue The new field value.
+	 */
+	<F> void applySetFunc (Object[] setFunc, F newFieldValue);
+	/**
+	 * Set the value of some field of this data.  The set function returns an instance {@code setResult(T)}.
 	 * @param setMFunc The field set function.
 	 * @param newFieldValue The new field value.
 	 */
-	boolean applySetMFunc (Object[] setMFunc, Object newFieldValue);
+	<F> boolean applySetMFunc (Object[] setMFunc, F newFieldValue);
 }
