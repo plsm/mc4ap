b3e233680f5445567788f1ea7dfed537387fb14d
Pedro Mariano <>
2014-03-14 13:38:13 +0000

    Change type of key attribute.

--- java/ui/KeyGenerator.java
+++ java/ui/KeyGenerator.java
@@ -11,17 +11,51 @@ package ui;
  */
 final public class KeyGenerator
 {
-	
+	/**
+	 *
+	 */
+	public static Key EMPTY = new Key () {
+		@Override
+		public String toString ()
+		{
+			return "EMPTY";
+		}
+	};
+	public static Key NOT_USED = new Key () {
+		@Override
+		public String toString ()
+		{
+			return "NOT_USED";
+		}
+	};
+			  
 	/**
 	 * Integer used to generate unique keys for {@code UIPanel} instances.
 	 */
 	private int KEY_GENERATOR = 0;
 	
 	
-	synchronized public String nextKey ()
+	synchronized public Key nextKey ()
 	{
-		String result;
-		result = "key" + (++KEY_GENERATOR);
-		return result;
+//		String result;
+//		result = "key" + (++KEY_GENERATOR);
+//		return result;
+		return new KeyInstance (++KEY_GENERATOR);
+	}
+	private class KeyInstance
+		implements Key
+	{
+		final int id;
+		final String result;
+		KeyInstance (int id)
+		{
+			this.id = id;
+			this.result = "key" + id;
+		}
+		@Override
+		public String toString ()
+		{
+			return result;
+		}
 	}
 }
